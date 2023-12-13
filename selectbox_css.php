6b9018663b34a7f12e2db1b1eb34c7edda13df2c
Husnain Ali <148160635+HusnainAli999@users.noreply.github.com>
2023-12-14 03:27:30 +0500

    Add files via upload

---
+++ selectbox_css.php
@@ -0,0 +1,24 @@
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    h1 {
+        background: black;
+        color: white;
+        padding: 20px;
+        text-align: center;
+    }
+
+    .main_owner_div_of_selectBox {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        width: 100vw;
+        height: 100px;
+    }
+</style>
+
+<h1>Select Box With Php Mysql Live Data Fetching</h1>
